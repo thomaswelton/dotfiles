cc628de17d5ef21cccd5ac5c22d93b9c61ecae77
thomaswelton <thomaswelton@me.com>
2014-04-22 03:26:51 +0100

    Install rvm

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,3 +5,8 @@ then
   echo "  Installing rvm for you."
   \curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles > /tmp/rvm-install.log
 fi
+
+source ~/.rvm/scripts/rvm
+
+rvm install ruby-2.0.0-p451
+rvm use ruby-2.0.0-p451 --default
