04aeed92f38c0629ac1ac8be7ea1cc079a964990
Hamilton Turner <hamiltont@gmail.com>
2014-06-04 13:41:33 -0400

    Correct plugin set for upstream

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -7,6 +7,6 @@ export ZSH_THEME="kphoen"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git docker common-aliases git-extras python rand-quote sbt scala screen svn wd tmux)
-# plugins=(bower colorize brew-cask composer gem git-extras npm osx terminalapp vagrant autojump brew git-flow)
+# plugins=(git docker common-aliases git-extras python rand-quote sbt scala screen svn wd tmux)
+plugins=(bower colorize brew-cask composer gem git-extras npm osx terminalapp vagrant autojump brew git-flow)
 
