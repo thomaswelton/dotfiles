e99b370d24d72985c0486c78fc903056f1238fce
Thomas Welton <thomaswelton@me.com>
2015-08-23 02:18:40 +0100

    Add show/hide files alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias artisan='php artisan'
 
 alias git=hub
 alias gittower='/Applications/Tower.app/Contents/MacOS/gittower'
+
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
